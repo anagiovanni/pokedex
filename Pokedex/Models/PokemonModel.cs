92c16965b7c24e20f04e9235e4fdd3616905ee2c
anagiovanni <anagiovanni66@gmail.com>
2024-02-22 12:29:46 -0300

    Finalização da classe pokemon

--- Pokedex/Models/PokemonModel.cs
+++ Pokedex/Models/PokemonModel.cs
@@ -1,7 +1,19 @@
 namespace Pokedex.Models;
-    public class PokemonModel
-    {
-         public int Numero { get; set; }
+public class PokemonModel
+{
+     public int Numero { get; set; }
+     public string Nome { get; set; }
+     public string Descricao { get; set; }
+     public string Especie { get; set; }
+     public List<string> Tipo { get; set; }
+     public double Altura { get; set; }
+     public double Peso { get; set; }
+     public double Imagem { get; set; }
 
-         public string? Nome { get; set; }
-    }
+     public PokemonModel()
+     {
+          Tipo = new List<string>();
+     }
+
+}
+
