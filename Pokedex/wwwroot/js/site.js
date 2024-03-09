0d7e6feaae1b38034ad02236cdbbfad8c8eaa9b0
anagiovanni <anagiovanni66@gmail.com>
2024-03-09 11:17:20 -0300

    Quase terminando a pokedex - até a página 157

--- Pokedex/wwwroot/js/site.js
+++ Pokedex/wwwroot/js/site.js
@@ -1,4 +1,23 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿function filter(type)
+{
+    let cards, i;
+    let count = 0;  
+    cards = document.getElementsByClassName("card");
+    buttons = document.getElementsByClassName("btn-filter");
+    for(i = 0; i < cards.length; i++)
+    {
+        cards[i].parentElement.style.display = 'none';
+        if (cards[i].classList.contains(type) || type === "all")
+        {
+            cards[i].parentElement.style.display = 'block';
+            count += 1;
+        };
+    };
+    for(i = 0; i < buttons.length; i++)
+    {
+        if(buttons[i].id == `btn-${type}`)
+        {
+            //Continue aqui Giovanni Futuro - Vá Pg 157
+        }
+    }
+}
